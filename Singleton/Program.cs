32cf06fd42596a93bf442ea9d2db42b40e29b2e8
jamil salim daruix <64331638+jamildaruix@users.noreply.github.com>
2020-05-01 14:36:22 -0300

    Criado o singleton para exemplo (#2)
    Co-authored-by: Jamil <jamil_salim_daruix@hotmail.com>

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -6,8 +6,16 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
 
-            Console.WriteLine("Hello World!");
+            if (singleton1 == singleton2)
+                Console.WriteLine("Classes são iguais");
+            else
+                Console.WriteLine("Classes nãos são iguais");
+
+            Console.WriteLine($"Guid create Singleton 1 {singleton1.Id}");
+            Console.WriteLine($"Guid create Singleton 2 {singleton2.Id}");
         }
     }
 }
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public Guid Id { get; private set; }
+
+        private Singleton() 
+        {
+            Id =  Guid.NewGuid();
+        }
+
+
+        private static Singleton _singleton;
+
+        public static Singleton GetInstance()
+        {
+            if (_singleton == null)
+                _singleton = new Singleton();
+
+            return _singleton;
+        }
+    }
+}
